bd9fa473ebda7a9b7307165b707ec8d75c582e8e
Silence_Lurker <slvccans@gmail.com>
2023-09-08 01:13:53 +0800

    add a filter to change character set

--- src/main/java/xyz/silencelurker/file/transport/controller/TransportController.java
+++ src/main/java/xyz/silencelurker/file/transport/controller/TransportController.java
@@ -1,79 +1,152 @@
 package xyz.silencelurker.file.transport.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.annotation.Resource;
-import xyz.silencelurker.file.transport.entity.RepositoryItem;
-import xyz.silencelurker.file.transport.entity.TransportItem;
-import xyz.silencelurker.file.transport.service.ITransportItemService;
-import xyz.silencelurker.file.transport.type.Type;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * @author Silence_Lurker
+ * @version 0.1.1-alpine
  */
-@CrossOrigin
-@RestController
+@Log4j2
 @RequestMapping("/transport")
+@RestController
 public class TransportController {
-    @Resource
-    private ITransportItemService transportItemService;
 
-    private static final String TYPE_FILE = "File";
-    private static final String TYPE_STRING = "String";
-    private static final String TYPE_FOLDER = "Folder";
+    private static final String DEFAULT_LOCATION = "./temp/";
+    private static final String TEST_OBJECT = "Test Object!";
 
-    @PostMapping("/upload")
-    public String upload(@RequestBody RepositoryItem repositoryItem) {
-        TransportItem transportItem = new TransportItem();
+    @GetMapping("/test")
+    public boolean test() throws IOException {
+        File testFile = new File(DEFAULT_LOCATION + "testFile.data");
+        log.info("执行测试");
 
-        var type = repositoryItem.getType();
-        var data = repositoryItem.getData();
-        var fileName = repositoryItem.getFileName();
-        var key = repositoryItem.getKey();
-        Type itemType;
+        if (testFile.exists()) {
+            FileInputStream inputStream = new FileInputStream(testFile);
 
-        if (TYPE_FOLDER.equalsIgnoreCase(type)) {
-            itemType = Type.Folder;
-        } else if (TYPE_FILE.equalsIgnoreCase(type)) {
-            itemType = Type.File;
-        } else if (TYPE_STRING.equalsIgnoreCase(type)) {
-            itemType = Type.String;
+            String data = new String(inputStream.readAllBytes());
+
+            inputStream.close();
+            if (data.equalsIgnoreCase(TEST_OBJECT)) {
+                return true;
+            }
+
+        }
+
+        new File(DEFAULT_LOCATION).mkdirs();
+
+        testFile.createNewFile();
+
+        FileOutputStream outputStream = new FileOutputStream(testFile);
+
+        outputStream.write(TEST_OBJECT.getBytes());
+
+        outputStream.close();
+
+        return true;
+
+    }
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<String> uploadFile(@RequestParam(required = false) String location, MultipartFile file)
+            throws IOException {
+
+        log.info("上传文件请求接收。");
+
+        File target;
+
+        File dir = new File(DEFAULT_LOCATION + location);
+
+        if (!(dir.exists())) {
+            log.info("目录不存在，尝试创建目录。");
+            dir.mkdirs();
+            log.info("目录创建成功。");
+        }
+
+        if (location == null) {
+            target = new File(DEFAULT_LOCATION + file.getOriginalFilename());
         } else {
-            itemType = Type.String;
+            target = new File(DEFAULT_LOCATION + location + file.getOriginalFilename());
+        }
+        if (target.exists()) {
+            return ResponseEntity.badRequest().body("File Already Exists!");
         }
 
-        transportItem.setData(data.getBytes());
-        transportItem.setFileName(fileName);
-        transportItem.setType(itemType);
+        var stream = file.getInputStream();
 
-        if (key == null || key.isEmpty()) {
-            return transportItemService.saveFile(transportItem.getData(), transportItem.getFileName());
-        }
+        FileOutputStream outputStream = new FileOutputStream(target);
 
-        transportItemService.saveFileWithKey(transportItem.getData(), transportItem.getFileName(), key);
-        return key;
+        // for (int i = stream.read(); i != -1; i = stream.read()) {
+        // outputStream.write(i);
+        // }
 
+        outputStream.write(stream.readAllBytes());
+
+        outputStream.close();
+        stream.close();
+
+        return ResponseEntity.ok().body("Upload Success!");
     }
 
-    @GetMapping("/data")
-    String getData(@RequestParam(required = false) String key) {
-        if (key == null || key.isEmpty()) {
-            return new String(transportItemService.getData());
-        }
-        return new String(transportItemService.getDataByKey(key));
+    @DeleteMapping("/deleteFile")
+    public ResponseEntity<String> deleteFile(@RequestParam(required = false) String location,
+            @RequestParam(required = false) String fileName) {
+
+        // TODO: Not Support Now!
+        return ResponseEntity.ok().body("Delete Success!(Testing)");
     }
 
-    @GetMapping("/fileName")
-    String getFileName(@RequestParam(required = false) String key) {
-        if (key == null || key.isEmpty()) {
-            return transportItemService.getFileName();
+    @GetMapping("/getFile")
+    public ResponseEntity<Resource> getFile(@RequestParam(required = false) String location,
+            @RequestParam(required = false) String fileName) {
+        FileSystemResource file;
+
+        log.info("检查目标文件信息");
+        if (fileName == null) {
+            file = new FileSystemResource(DEFAULT_LOCATION + "test.jpg");
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(file);
+        } else if (location == null) {
+            file = new FileSystemResource(DEFAULT_LOCATION + fileName);
+        } else {
+            file = new FileSystemResource(DEFAULT_LOCATION + location + fileName);
         }
-        return transportItemService.getFileNameByKey(key);
+
+        MediaType type = MediaType.MULTIPART_FORM_DATA;
+
+        if (!file.exists()) {
+            log.info("未找到匹配文件，导向至错误页面");
+            file = new FileSystemResource("./page/NotFound.html");
+            return ResponseEntity.badRequest().contentType(MediaType.TEXT_HTML).body(file);
+        }
+
+        if (fileName.contains(".jpg") || fileName.contains(".jpeg")) {
+            type = MediaType.IMAGE_JPEG;
+        } else if (fileName.contains(".gif")) {
+            type = MediaType.IMAGE_GIF;
+        } else if (fileName.contains(".png")) {
+            type = MediaType.IMAGE_PNG;
+        } else if (fileName.contains(".md")) {
+            type = MediaType.TEXT_MARKDOWN;
+        } else if (fileName.contains(".html")) {
+            type = MediaType.TEXT_HTML;
+        }
+
+        return ResponseEntity.ok().contentType(type).body(file);
     }
+
 }
--- src/main/java/xyz/silencelurker/file/transport/filter/CharacterFilter.java
+++ src/main/java/xyz/silencelurker/file/transport/filter/CharacterFilter.java
@@ -2,6 +2,8 @@ package xyz.silencelurker.file.transport.filter;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Component;
+
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,17 +12,19 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 
 /**
+ * 该过滤器用于设置响应字符集编码
+ * 
  * @author Silence_Lurker
  */
+@Component
 @WebFilter("/*")
-public class TestFilter implements Filter {
+public class CharacterFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        System.out.println(request.getRemoteHost());
-
+        response.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
 
