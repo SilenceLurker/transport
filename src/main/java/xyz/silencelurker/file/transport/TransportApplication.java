54fc73705711577a10c5db78965522733dbd24e6
Silence_Lurker <10330631+SilenceLurker@user.noreply.gitee.com>
2023-09-19 23:34:09 +0800

    Add Redis Status Check

--- src/main/java/xyz/silencelurker/file/transport/TransportApplication.java
+++ src/main/java/xyz/silencelurker/file/transport/TransportApplication.java
@@ -1,9 +1,14 @@
 package xyz.silencelurker.file.transport;
 
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import xyz.silencelurker.file.transport.conf.RedisStatusCheckConfig;
 
 /**
  * @author Silence_lurker
@@ -14,6 +19,15 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class TransportApplication {
 
 	public static void main(String[] args) {
+
+		try {
+			RedisTemplate<String, Object> template = new RedisTemplate<>();
+			var valueOps = template.opsForValue();
+			valueOps.set("test-Key" + UUID.randomUUID().toString(), "test-VALUE", 1);
+		} catch (Exception e) {
+			RedisStatusCheckConfig.redisStatus = false;
+		}
+
 		SpringApplication.run(TransportApplication.class, args);
 	}
 
---
+++ src/main/java/xyz/silencelurker/file/transport/conf/RedisStatusCheckConfig.java
@@ -0,0 +1,8 @@
+package xyz.silencelurker.file.transport.conf;
+
+/**
+ * @author Silence_Lurker
+ */
+public class RedisStatusCheckConfig {
+    public static boolean redisStatus = true;
+}
